44b90a694b60d1beb6429b6d33f40b1050725ca2
Ademodsx <157985548+Ademodsx@users.noreply.github.com>
2024-06-10 00:28:55 +0700

    Update script.js

--- upload/script.js
+++ upload/script.js
@@ -1,42 +1,43 @@
-const form = document.getElementById('uploadForm');
-const fileInput = document.getElementById('fileInput');
+const token = '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';
+const repoOwner = 'Ademods';
+const repoName = 'Music';
+const uploadURL = `https://api.github.com/repos/${repoOwner}/${repoName}/music/`;
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const file = fileInput.files[0];
-  if (file) {
-    uploadToGitHub(file);
-  } else {
-    alert('Please select a file');
-  }
-});
+document.getElementById('uploadBtn').addEventListener('click', uploadFile);
 
-function uploadToGitHub(file) {
-  // Ganti dengan kode untuk mengakses token GitHub
-  const githubToken = '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';
+function uploadFile() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
 
-  const formData = new FormData();
-  formData.append('file', file);
-
-  fetch('https://api.github.com/Ademodsx/Music/music', {
-    method: 'PUT',
-    headers: {
-      Authorization: `token ${githubToken}`
-    },
-    body: JSON.stringify({
-      message: 'Upload file',
-      content: btoa(unescape(encodeURIComponent(file))),
-    })
-  })
-  .then(response => {
-    if (response.ok) {
-      window.location.href = 'https://music.ademods.my.id/music/' + file.name;
-    } else {
-      throw new Error('Error uploading file to GitHub');
+    if (!file) {
+        alert('Please select a file.');
+        return;
     }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    alert('An error occurred while uploading the file');
-  });
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        const fileContent = reader.result.split(',')[1];
+        const fileName = file.name;
+
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                'Authorization': `token ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: 'Upload file',
+                content: fileContent
+            })
+        };
+
+        fetch(uploadURL + fileName, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                const fileURL = data.content.download_url;
+                window.location.href = `https://music.ademods.my.id/music/${fileName}`;
+            })
+            .catch(error => console.error('Error:', error));
+    };
 }
